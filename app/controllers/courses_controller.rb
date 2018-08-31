db340ffe4fa375562b41b5e4462b4834e7a5bc36
Bradley Bain <brad_bain@live.com>
2018-08-31 16:17:08 -0700

    Create course section search endpoint

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,4 +3,68 @@ class CoursesController < ApplicationController
     @academic_terms = AcademicTerm.all
     @departments = Department.all
   end
+
+  def search_course_sections
+    if params[:academic_term].nil? || params[:academic_term].empty?
+      return render :json => {error: "No academic term specified"}, :status => :bad_request
+    end
+
+    term_key = params[:academic_term].split.reverse.join(';')
+    department_code = Department.find_by(:name => params[:department]).code unless params[:department].empty?
+    instructor_name =  params[:instructor].strip unless params[:instructor].empty?
+    number = params[:number].to_i rescue nil unless params[:number].empty?
+    keywords = params[:keywords].split rescue nil unless params[:keywords].empty?
+
+    schools = {
+        :pomona => params[:pomona] || false,
+        :claremont_mckenna => params[:claremont_mckenna] || false,
+        :harvey_mudd => params[:harvey_mudd] || false,
+        :scripps => params[:scripps] || false,
+        :pitzer => params[:pitzer] || false
+    }
+    schools.select! {|k,v| v}
+
+    days = {
+        :monday => params[:monday] || false,
+        :tuesday => params[:tuesday] || false,
+        :wednesday => params[:wednesday] || false,
+        :thursday => params[:thursday] || false,
+        :friday => params[:friday] || false
+    }
+    days.select! {|k,v| v}
+
+    matches_query = CourseSection
+          .joins(:academic_term)
+          .joins(:course => [:departments])
+          .where(:academic_terms => {:key => term_key})
+
+    if(instructor_name)
+      matches_query = matches_query
+          .joins(:instructors)
+          .where(:instructors => {:name => instructor_name})
+    end
+
+    if(days.length > 0)
+      matches_query = matches_query
+          .joins(:course_meeting_details)
+          .where(:course_meeting_details => days)
+    end
+
+    if(department_code)
+      matches_query = matches_query
+          .where(:courses => {:departments => {:code => department_code}})
+    end
+
+    if(number)
+      matches_query = matches_query
+          .where(:courses => {:number => number })
+    end
+
+    matches = matches_query.all
+    if(keywords)
+      matches = matches.select { |section| keywords.any? { |keyword| section.course.name.include? keyword } }
+    end
+
+    render :json => matches.to_json, :status => :ok
+  end
 end
