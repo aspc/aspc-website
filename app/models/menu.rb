8654b10ecf5d8b048769b96ba49bc06548e50a28
Pavle Rohalj <pavlerohalj@users.noreply.github.com>
2018-10-22 13:38:47 -0700

    Update menu model

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,7 +1,7 @@
 class Menu < ApplicationRecord
   has_many :menu_items
 
-  enum dining_hall: [ :frank, :frary, :claremont_mckenna, :harvey_mudd, :scripps, :pitzer ]
+  enum dining_hall: [ :frank, :frary, :oldenborg, :claremont_mckenna, :harvey_mudd, :scripps, :pitzer ]
   enum day: [ :sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday ]
   enum meal_type: [ :breakfast, :lunch, :dinner, :brunch ]
 end
