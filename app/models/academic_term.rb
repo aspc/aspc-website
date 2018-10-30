549e0eb1b1c7cfc318d298d1a9063f3fab745ac9
Bradley Bain <brad_bain@live.com>
2018-10-29 19:44:40 -0700

    Create current_academic_year helper method on AcademicTerm

--- app/models/academic_term.rb
+++ app/models/academic_term.rb
@@ -1,2 +1,7 @@
 class AcademicTerm < ApplicationRecord
+
+  ##
+  # Returns the two latest semesters
+
+  scope :current_academic_year, -> { order(:year => :desc).order(:session => :asc).first(2) }
 end
