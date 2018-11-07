dbe7c7af2b9a6a47bbc8fdd0d7d97ccf5b1d25a5
Adam <agsmguo@gmail.com>
2018-11-07 09:16:24 -0800

    Display current meal of day based on time

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,7 +1,32 @@
 class MenuController < ApplicationController
   def index
-    @meal_type = "lunch"
-    @day = Date.today.wday
+	@day = Date.today.wday
+
+	require "tzinfo"
+
+	timezone_name = "US/Pacific"
+
+	timezone = TZInfo::Timezone.get(timezone_name)
+	offset_in_hours = timezone.current_period.utc_total_offset_rational.numerator
+	offset = "'%+.2d:00" % offset_in_hours
+
+	hour = Time.now.getlocal(offset)
+	puts ["hour", hour, Time.now.getlocal(offset)]
+	if @day == 0 or @day == 6
+		if hour < 14
+			@meal_type = "brunch"
+		else
+			@meal_type = "dinner"
+		end
+	else
+		if hour < 10
+			@meal_type = "breakfast"
+		elsif hour < 16
+			@meal_type = "lunch"
+		else
+			@meal_type = "dinner"
+		end
+	end
   end
 
   def show
