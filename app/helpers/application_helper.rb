0759f4e15795298a9da384d271ed1dca8577fb90
Pavle Rohalj <pavlerohalj@users.noreply.github.com>
2019-01-16 02:30:17 -0800

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def person_photo person
+    if person.image.present?
+      image_tag user.image_url
+    else
+      image_tag "static/placeholder.png"
+    end
+  end
 end
