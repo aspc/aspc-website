af02273f33d00ea4c900dcfd1dbd4108a79640f6
Pavle Rohalj <pavlerohalj@users.noreply.github.com>
2019-01-16 02:39:42 -0800

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
-  def person_photo person
+  def person_photo (person)
     if person.image.present?
-      image_tag user.image_url
+      image_tag person.image
     else
       image_tag "static/placeholder.png"
     end
