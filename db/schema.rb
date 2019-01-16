a82548cc33ea4a6de7bc6eec9a312886588756d1
Pavle Rohalj <pavlerohalj@users.noreply.github.com>
2019-01-16 01:08:49 -0800

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_16_085004) do
+ActiveRecord::Schema.define(version: 2019_01_16_090709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -210,7 +210,7 @@ ActiveRecord::Schema.define(version: 2019_01_16_085004) do
   create_table "people", force: :cascade do |t|
     t.string "name"
     t.string "position"
-    t.integer "role"
+    t.integer "role", default: 0
     t.string "email"
     t.text "biography"
     t.datetime "created_at", null: false
