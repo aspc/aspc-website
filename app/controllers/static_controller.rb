3df3430431d28d07cce4c7e17dbf32c31e173b8e
Pavle Rohalj <pavlerohalj@users.noreply.github.com>
2019-01-16 21:52:44 -0800

    Update static_controller.rb

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,6 @@ class StaticController < ApplicationController
   def index
     @announcements = Announcement.all
     time = Time.parse(Time.now.in_time_zone("Pacific Time (US & Canada)").strftime('%Y-%m-%d %H:%M:%S')).to_s(:db)
-    @events = Event.where("start >= ?", time).where(:status => :approved).order(:start).take(3)
+    @events = Event.where("start >= ?", time).where(:status => :approved).order(:start).take(6)
   end
 end
