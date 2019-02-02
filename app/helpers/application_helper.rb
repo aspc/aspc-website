7775e2aff7bfdf8fd5e9ad723c3f5f1bb2cde260
Pavle Rohalj <pavlerohalj@users.noreply.github.com>
2019-02-02 03:16:52 -0800

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def google_calendar_add_url (event)
+    date_time = event.start.strftime('%Y%m%dT%H%M%S') + '/' + event.end.strftime('%Y%m%dT%H%M%S')
+    return "https://calendar.google.com/calendar/r/eventedit?text=#{CGI.escape event.name}&dates=#{date_time}&details=#{CGI.escape event.description}&location=#{CGI.escape event.location}"
+  end
 end
