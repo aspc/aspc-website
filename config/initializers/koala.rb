a24ff3a29d04cb7e466d6e4713fdfcd334be73ac
Pavle Rohalj <pavlerohalj@users.noreply.github.com>
2019-02-19 02:24:22 -0800

    Add Koala initializer

---
+++ config/initializers/koala.rb
@@ -0,0 +1,5 @@
+Koala.configure do |config|
+  config.oauth_callback_url= "https://pomonastudents.org/"
+  config.app_id = 304242016900984
+  config.app_secret = "e1a63d3689a29d89a2be03b6598de8bc"
+end
