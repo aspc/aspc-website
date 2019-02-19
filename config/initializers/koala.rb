cc2601dc4e0768f80fd18a61abfc2fc8be1d4e88
Pavle Rohalj <pavlerohalj@users.noreply.github.com>
2019-02-19 15:51:22 -0800

    Update koala.rb

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,5 +1,5 @@
 Koala.configure do |config|
   config.oauth_callback_url= "https://pomonastudents.org/"
-  config.app_id = 304242016900984
-  config.app_secret = "e1a63d3689a29d89a2be03b6598de8bc"
+  config.app_id = ENV['FACEBOOK_API_APP_ID']
+  config.app_secret = ENV['FACEBOOK_API_APP_SECRET']
 end
