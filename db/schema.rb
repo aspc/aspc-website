547560f09a93916db4f93f55cb067992ff91bd04
Pavle Rohalj <pavlerohalj@users.noreply.github.com>
2019-02-19 02:22:51 -0800

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_16_090709) do
+ActiveRecord::Schema.define(version: 2019_02_18_203239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -173,6 +173,7 @@ ActiveRecord::Schema.define(version: 2019_01_16_090709) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "college_affiliation", default: 0, null: false
+    t.integer "source"
     t.index ["college_affiliation"], name: "index_events_on_college_affiliation"
     t.index ["status"], name: "index_events_on_status"
   end
