8d19eab1f0e92be07b07bffc0b67280f6258239e
Bradley Bain <brad_bain@live.com>
2019-03-10 13:58:19 -0700

    Polish new page admin action

--- app/admin/static.rb
+++ app/admin/static.rb
@@ -1,6 +1,12 @@
 ActiveAdmin.register Static do
   menu :priority => 3, :label => "Custom Pages"
 
+  # Change "new" button name
+  config.clear_action_items!
+  action_item :only => :index do
+    link_to "New Page" , new_admin_static_path
+  end
+
   permit_params :title, :subtitle
 
   index :title => "Static Pages" do
@@ -19,6 +25,12 @@ ActiveAdmin.register Static do
   controller do
     def show
       redirect_to static_page_path
+    end
+
+    def create
+      create! do |format|
+        format.html { redirect_to edit_admin_static_path(:id => resource.id) }
+      end
     end
   end
 
