28eef7dfd1ed5be86701e47dbc1ae526671f45c0
Bradley Bain <brad_bain@live.com>
2019-03-28 22:37:38 -0700

    Move avatar_photo helper to static_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def avatar_photo (person)
-    if person.image.attached?
-      image_tag person.image
-    else
-      image_tag "static/placeholder.jpg"
-    end
-  end
 end
