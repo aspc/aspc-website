56a9176d0e96e6aded1ea6f85ebd1be563225113
Bradley Bain <brad_bain@live.com>
2019-04-05 10:07:58 -0700

    Allow admin to change user role in panel

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,7 +1,7 @@
 user_page = Proc.new do
   menu parent: "Models"
 
-  permit_params :email, :first_name, :is_cas_authenticated, :is_admin, :school
+  permit_params :email, :first_name, :is_cas_authenticated, :is_admin, :role, :school
 end
 
 ActiveAdmin.register User, :namespace => :admin, &user_page
