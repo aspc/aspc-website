ba002777e8d784ab6023fa45136814ffc93e2b70
Adam Guo <agsmguo@gmail.com>
2019-04-05 22:48:40 -0700

    Require users to be logged in to submit housing reviews

--- app/controllers/housing_reviews_controller.rb
+++ app/controllers/housing_reviews_controller.rb
@@ -1,5 +1,6 @@
 class HousingReviewsController < InheritedResources::Base
   before_action :set_housing_room
+  before_action :authenticate_user!, only: [:create, :new, :destroy]
 
   def index
     @housing_reviews = @housing_room.housing_reviews
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2019_04_03_222622) do
-=======
 ActiveRecord::Schema.define(version: 2019_04_05_190517) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
