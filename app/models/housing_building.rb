8a60f21c43fd2a8212795532ec0b3a8417a09d38
Bradley Bain <brad_bain@live.com>
2019-04-11 20:14:28 -0700

    Associate an image with building model and allow upload on admin panel

--- app/models/housing_building.rb
+++ app/models/housing_building.rb
@@ -2,5 +2,7 @@ class HousingBuilding < ApplicationRecord
   has_many :housing_rooms
   has_many :housing_suites
 
+  has_one_attached :image
+
   validates :name, :presence => true, :allow_blank => false, :uniqueness => true
 end
