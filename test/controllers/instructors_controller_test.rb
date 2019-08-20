4c83315d75ad9f6d3dd8e9e53ddd45f1408fa419
Bradley Bain <brad_bain@live.com>
2019-08-20 16:18:45 -0500

    Delete old testing framework

--- test/controllers/instructors_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class InstructorsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get instructors_show_url
-    assert_response :success
-  end
-
-end
