a2923513a1e6e3b97448f4e41c85c6cbcebe9e98
Adam Guo <agsmguo@gmail.com>
2020-02-17 12:43:55 -0800

    Add basic event filtering to engage_events_service.rb

--- app/services/engage_events_service.rb
+++ app/services/engage_events_service.rb
@@ -21,9 +21,18 @@ class EngageEventsService
         response = HTTParty.get(Rails.application.credentials.engage_api[:url], :headers => headers, :query => query)
 
         engage_events = response["items"]		# @events is a list of hashes, each of which is an event
+        urls = Array.new       # keep track of the event url's that are pulled
 
         # save to db on the fly, then let events_controller pull from db
         engage_events.each do |event|
+            # filter out some basic events that are definitely not for the public
+            filter = ["Sunday Practice", "General Meetings"]
+            if filter.include? event["eventName"].strip
+                next
+            end
+
+            urls << event["eventUrl"]
+
             # if event is marked for organisation only, then skip
             if event["typeName"] == "Organization Only"
                 next
@@ -33,7 +42,7 @@ class EngageEventsService
             if Event.exists?(details_url: event["eventUrl"])
                 next
             end
-                
+
             # if an event starts 12pm PST, the controller expects an input of 12pm UTC (rather than 8pm UTC)
             # need to manually offset time
             offset = Time.now.in_time_zone("America/Los_Angeles").utc_offset
@@ -56,6 +65,23 @@ class EngageEventsService
             @event.approve!
 
             res = @event.save!
+        end
+
+        # get engage events already in database and check if they still exist on engage
+        # if not, then delete
+        start_ts = Time.at(start_time / 1000)
+        end_ts = Time.at(end_time / 1000)
+        Event.where("start BETWEEN ? AND ?", start_ts, end_ts).each do |event|
+
+            # check if the event is from engage
+            if (event[:details_url].include? "https://claremont.campuslabs.com/engage/event/")
+
+                # check if the event was on the list pulled from engage
+                if not (urls.include? event[:details_url])
+                    puts event
+                    event.destroy
+                end
+            end
         end
     end
 end
