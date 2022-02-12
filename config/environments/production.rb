39578e3174de29529abfae135f55525c372b822b
Adam Guo <agsmguo@gmail.com>
2022-02-11 21:10:08 -0800

    Change production ActionMailer delivery method to smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # SMTP settings for gmail
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
