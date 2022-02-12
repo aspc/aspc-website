7f53caeccd1d3280dfaf33ff137cfd462314e2a2
Adam Guo <agsmguo@gmail.com>
2022-02-11 21:04:53 -0800

    Rename events_email.html.erb

--- app/mailers/events_mailer.rb
+++ app/mailers/events_mailer.rb
@@ -1,6 +1,9 @@
 class EventsMailer < ApplicationMailer
 
   def event_notification_email(event)
+    Rails.logger.info "event_notification_email"
+    Rails.logger.info event
+
     @event = event
     @recipient = "sgab2018@mymail.pomona.edu"
     mail(to: @recipient, subject: @event.name)
