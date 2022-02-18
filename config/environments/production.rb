c32b08469a9c10c5656df427635c623aa8ca1c08
Adam Guo <agsmguo@gmail.com>
2022-02-17 19:47:23 -0800

    Add host url to actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # SMTP settings for gmail
+  config.action_mailer.default_url_options = { :host => "www.pomonastudents.org" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
