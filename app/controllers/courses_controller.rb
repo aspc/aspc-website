1431421342d9431bbb8335c675a497dccf97708d
Adam Guo <agsmguo@gmail.com>
2022-03-03 14:12:53 -0800

    Remove comments

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -153,20 +153,16 @@ class CoursesController < ApplicationController
 
     consider_time = false
     if not start_hour.nil?  # if user specifies start time
-      # start_time = Time.new(1970, 1, 1, start_hour, start_minute, "+00:00")
       start_time = ActiveSupport::TimeZone['UTC'].local(1970, 1, 1, start_hour, start_minute).to_datetime
     
       if end_hour.nil? then end_hour = 23 end   # if user doesn't specify end time, set default value to display all classes after start time
-      # end_time = Time.new(1970, 1, 1, end_hour, end_minute, "+00:00")
       end_time = ActiveSupport::TimeZone['UTC'].local(1970, 1, 1, end_hour, end_minute).to_datetime
       consider_time = true
 
     elsif not end_hour.nil?   # if user only specifies end time but not start time
-      # end_time = Time.new(1970, 1, 1, end_hour, end_minute, "+00:00")
       end_time = ActiveSupport::TimeZone['UTC'].local(1970, 1, 1, end_hour, end_minute).to_datetime
       
       if start_hour.nil? then start_hour = 0 end   # double check that user did not set start time, set default value to display all classes before end time
-      # start_time = Time.new(1970, 1, 1, start_hour, start_minute, "+00:00")
       start_time = ActiveSupport::TimeZone['UTC'].local(1970, 1, 1, start_hour, start_minute).to_datetime
       consider_time = true
     end
