04cdff756e3f302151a25af235c96207c3fc3141
Chau Vu <vuhachau2412@gmail.com>
2024-02-09 15:07:20 -0800

    Upload images to reviews
    Can only upload from admin as of now. Todo: need to check why we can't get image address from Active Storage from frontend

--- app/admin/housing_review.rb
+++ app/admin/housing_review.rb
@@ -1,7 +1,13 @@
 housing_review_page = Proc.new do
   menu parent: "Models"
 
-  permit_params :overall_rating, :layout_rating, :temperature_rating, :comments, :housing_room_id
+  form do |f|
+    f.inputs
+    f.input :image, :as => :file
+    f.actions
+  end
+
+  permit_params :overall_rating, :layout_rating, :temperature_rating, :comments, :housing_room_id, :image
 end
 
 ActiveAdmin.register HousingReview, :namespace => :admin, &housing_review_page
--- app/models/housing_review.rb
+++ app/models/housing_review.rb
@@ -3,6 +3,7 @@ class HousingReview < ApplicationRecord
   belongs_to :user, :optional => true
 
   validates :housing_room, presence: true
+  has_one_attached :image
 
   def written_at
     self.created_at.strftime("%B %Y")
