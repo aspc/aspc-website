5b93e3329620c229fb6603a797e6295080fefdd1
Evelyn Hasama <ehasama11@gmail.com>
2024-04-01 19:48:06 -0700

    limit image size to 5MB each

--- app/models/housing_review.rb
+++ app/models/housing_review.rb
@@ -4,8 +4,19 @@ class HousingReview < ApplicationRecord
 
   validates :housing_room, presence: true
   has_many_attached :images
+  validate :validate_image_size
 
   def written_at
     self.created_at.strftime("%B %Y")
+  end
+
+  private
+
+  def validate_image_size
+    images.each do |image|
+      if image.byte_size > 5.megabytes
+        errors.add(:images, "A file uploaded exceeds the maximum allowed size of 5MB.")
+      end
+    end
   end
 end
