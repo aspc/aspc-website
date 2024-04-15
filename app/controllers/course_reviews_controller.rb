4a9bd09ed39d8548f9fad72b2e8ff86af11eafa2
Chau Vu <vuhachau2412@gmail.com>
2024-04-14 19:11:16 -0700

    Add sorting functionality to course reviews

--- app/controllers/course_reviews_controller.rb
+++ app/controllers/course_reviews_controller.rb
@@ -110,6 +110,10 @@ class CourseReviewsController < ApplicationController
     #     .where("name ILIKE ANY ( array[?] )", keywords_query)
     # end
 
+    if params[:sort_button]
+      matches_query = matches_query.sort_by {|course| -course.overall_rating}
+    end
+
     # server response
     @courses = matches_query.uniq
     respond_to do |format|
